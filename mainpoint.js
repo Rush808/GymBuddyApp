545b295168d272d2a0c40d2330e8053b23275fd1
Xavier.D <167911556+XavXavChess@users.noreply.github.com>
2024-09-13 12:02:01 -1000

    Void .js. Main file set

--- mainpoint.js
+++
@@ -1,5 +0,0 @@
-const navbar = document.getElementById('navbar');
-const colorPicker = document.getElementById('color-picker');
-colorPicker.addEventListener('change', (e) => {
-  navbar.style.backgroundColor = e.target.value;
-});
